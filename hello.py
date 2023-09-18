7624a41d63a5cb7ea8466f5ea3311080be7fa586
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2023-09-18 16:56:32 -0300

    imprime usando argumentos da cli

--- hello.py
+++ hello.py
@@ -18,15 +18,43 @@ Execucao:
 """
 
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Marcelo Garcia"
 __licensa__ = "Unlicense"
 
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+sys_argv = sys.argv[1:]
 
+#definindo argumentos que o programa espera receber
+arguments = {
+	"lang": None,
+	"count": 1,
+}
+
+
+for arg in sys_argv:
+	#TO DO: tratar ValueError
+	key, value = arg.split("=")
+	key = key.lstrip("-").strip()
+	value = value.strip()
+	if key not in arguments:
+		print(f"'{key}': Invalid options")
+		sys.exit() #para a execução do programa
+	arguments[key]=value
+
+
+current_language = arguments["lang"]
+if current_language is None:
+	#TO DO: usar repetição
+	if "LANG" in os.environ:
+		current_language = os.getenv("LANG")	
+	else:
+		current_language = input("Choose a language: ")
+
+current_language = current_language[:5]
 
 msg = {
 	"pt_BR": "Olá, mundo!",
@@ -36,7 +64,7 @@ msg = {
 	"en_US": "Hello, world!",
 }
 
-print(msg[current_language])
+print(msg[current_language] * int(arguments["count"]))
 
 
 
